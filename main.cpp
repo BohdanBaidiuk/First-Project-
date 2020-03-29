8120f41492261c845230c96e22bbd9305bdaac35
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2020-03-29 16:32:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#define CURL_STATICLIB
+#include "curl/curl.h"
+#include "findUrl.hpp"
+#include "Regex.hpp"
+#include"optionKonsole.hpp"
+#include <string>
+#include <iostream>
+#include <vector>
 
+
+int main(int argc, char **argv) {
+  std::string url = "google.com";
+  size_t search_depth_url = 4;
+  if (Check_Url(url)) {
+    find_url(url);
+  } else {
+    std::cout << "Not correct input url" << std::endl;
+  }
+  return 0;
+}
