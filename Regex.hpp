369a3d5bf42c1f3c292d7a3f8a02fc08e8e6beaf
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2020-05-01 17:41:38 +0300

    Update Regex.hpp

--- Regex.hpp
+++ Regex.hpp
@@ -1,40 +1,52 @@
 #pragma once
-#include "ThreadPool.hpp"
+#include "ContentUrl.hpp"
 #include "findUrl.hpp"
 #include <algorithm>
-#include <iostream>
 #include <iterator>
 #include <map>
+#include <regex>
 #include <string>
-#include <thread>
-#include <utility>
+#include <vector>
 
-std::map<std::string, std::pair<bool, size_t>> https;
-std::map<std::string, std::pair<bool, size_t>> http;
-
-void print_content(
-    const std::map<std::string, std::pair<bool, size_t>> &content) {
-  std::for_each(content.begin(), content.end(), [](const auto &p) {
-    std::cout << p.first << " --depth-- = " << p.second.second << std::endl;
-  });
+bool Check_Url(std::string arg_url) {
+  std::regex Ceck_xRegEx("(https://[\\w-.]+)(\.)([a-z]{2,5})");
+  if (std::regex_match(arg_url, Ceck_xRegEx)) {
+    return true;
+  }
+  return false;
 };
 
-void search_urls(size_t search_depth_url, std::string arg_url) {
-  std::string content;
-  ThreadPool pool{4};
+void parseContent(std::string str,
+                  std::map<std::string, std::pair<bool, size_t>> &arg_https,
+                  std::map<std::string, std::pair<bool, size_t>> &arg_http,
+                  size_t depth, size_t &search_depth_url) {
+  std::regex reg_safe("(https://[\\w-./]+)");
+  std::regex reg_unsafe("(http://[\\w-./]+)");
+  std::sregex_iterator currentSafe(str.begin(), str.end(), reg_safe);
+  std::sregex_iterator lastSafe;
+  while (currentSafe != lastSafe) {
+    std::smatch match = *currentSafe;
+    if (arg_https.find(match.str()) == arg_https.end()) {
+      if (search_depth_url == depth) {
+        arg_https[std::move(match.str())] = {true, search_depth_url};
+      } else {
+        arg_https[std::move(match.str())] = {false, depth + 1};
+      }
+    }
+    currentSafe++;
+  }
 
-  decltype(https.begin()) it;
-  https[arg_url] = {false, 0};
-
-  /*
-  if (search_depth_url>0) {
-          while ((it = std::find_if(https.begin(), https.end(),
-                  [](auto& p) {return p.second.first == false; })) !=
-  https.end()) { it->second.first = true; content = find_url(it->first);
-                  parseContent(content, https, http, it->second.second,
-  search_depth_url);
-          }
-  }*/
-  print_content(https);
-  print_content(http);
+  std::sregex_iterator currentUnsafe(str.begin(), str.end(), reg_unsafe);
+  std::sregex_iterator lastUnsafe;
+  while (currentUnsafe != lastUnsafe) {
+    std::smatch match = *currentUnsafe;
+    if (arg_http.find(match.str()) == arg_http.end()) {
+      if (search_depth_url == depth) {
+        arg_http[std::move(match.str())] = {true, search_depth_url};
+      } else {
+        arg_http[std::move(match.str())] = {false, depth + 1};
+      }
+    }
+    currentUnsafe++;
+  }
 }
